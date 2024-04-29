e141a484f0ac5e85556b5aac34a8fa1f375c373f
Andrey Cadmo <andreycadmo06@gmail.com>
2024-04-29 10:29:31 -0300

    adicionando a seção para os jogos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../../components/header/header.component';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { CardGameComponent } from "../../components/card-game/card-game.component";
+
+export type gamelist = {
+  image: string;
+  name: string;
+  price: string;
+}; 
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  imports: [CommonModule, CardGameComponent],
+  templateUrl: "./home.component.html",
+  styleUrl: "./home.component.css",
 })
 export class HomeComponent {
-
+  cards: gamelist[] = [
+    {
+      image: "../../../../../assets/games/tiro2077.png",
+      name: "Tiro 2077",
+      price: "GRATUITO",
+    },
+    {
+      image: "../../../../../assets/games/cacadaCruel.png",
+      name: "Caçada Cruel",
+      price: "GRATUITO",
+    },
+    {
+      image: "../../../../../assets/games/esquadraoElite.png",
+      name: "Esquadrão de Elite",
+      price: "GRATUITO",
+    },
+    {
+      image: "../../../../../assets/games/furiaFlonteirica.png",
+      name: "Fúria Flonteiriça",
+      price: "GRATUITO",
+    },
+    {
+      image: "../../../../../assets/games/blitzTatica.png",
+      name: "Blitz Tática",
+      price: "GRATUITO",
+    }
+  ];
 }
