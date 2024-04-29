e8a00c393f5cd100cc04d4b4ec52b104e4aad627
Andrey Cadmo <andreycadmo06@gmail.com>
2024-04-29 01:49:41 -0300

    criando uma padronização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  <app-header></app-header>
+  <section class="router-content">
+    <router-outlet/>
+  </section>
+   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'DeadShot';
+  title = "DeadShot";
 }
