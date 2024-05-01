dc5f06272db476f53427cdefa0f492ed91822ee9
Andrey Cadmo <andreycadmo06@gmail.com>
2024-04-30 23:17:23 -0300

    adicionando o id para os jogos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./pages/home/home.component";
+import { JogoComponent } from "./pages/jogo/jogo.component";
 
 export const routes: Routes = [
   { path: "home", component: HomeComponent },
+  { path: "jogo/:id", component: JogoComponent },
   { path: "", redirectTo: "home", pathMatch: "full" },
 ];
