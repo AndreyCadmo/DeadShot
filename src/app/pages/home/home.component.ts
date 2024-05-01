8c9f50500067ed99514f3957ebb53318f4972274
Andrey Cadmo <andreycadmo06@gmail.com>
2024-04-30 23:21:10 -0300

    adicionando o ID aos dados do card e colocando a função para mudar de imagem

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CardGameComponent } from "../../components/card-game/card-game.component";
 
@@ -6,6 +6,7 @@ export type gamelist = {
   image: string;
   name: string;
   price: string;
+  id: string;
 };
 
 @Component({
@@ -18,37 +19,43 @@ export type gamelist = {
 export class HomeComponent {
   imageUrl = "../../../../../assets/notice/noticeOne.png";
 
-  mudarImage =  (image: string) => {
-    if(this.imageUrl != null){
+  mudarImage = (image: string) => {
+    if (this.imageUrl != null) {
       this.imageUrl = image;
     }
   };
 
   cards: gamelist[] = [
     {
-      image: "../../../../../assets/games/tiro2077.png",
+      image: "../../../../../assets/games/tiro2077/tiro2077.png",
       name: "Tiro 2077",
       price: "GRATUITO",
+      id: "1",
     },
     {
-      image: "../../../../../assets/games/cacadaCruel.png",
+      image: "../../../../../assets/games/cacadaCruel/cacadaCruel.png",
       name: "Caçada Cruel",
       price: "GRATUITO",
+      id: "2",
     },
     {
-      image: "../../../../../assets/games/esquadraoElite.png",
+      image: "../../../../../assets/games/esquadraoElite/esquadraoElite.png",
       name: "Esquadrão de Elite",
       price: "GRATUITO",
+      id: "3",
     },
     {
-      image: "../../../../../assets/games/furiaFlonteirica.png",
+      image:
+        "../../../../../assets/games/furiaFlonteirica/furiaFlonteirica.png",
       name: "Fúria Flonteiriça",
       price: "GRATUITO",
+      id: "4",
     },
     {
-      image: "../../../../../assets/games/blitzTatica.png",
+      image: "../../../../../assets/games/blitzTatica/blitzTatica.png",
       name: "Blitz Tática",
       price: "GRATUITO",
+      id: "5",
     },
   ];
 }
