8378c5b09d7e73ba1e2e429ce749901361929295
Andrey Cadmo <andreycadmo06@gmail.com>
2024-04-30 23:19:12 -0300

    adicionando os importes para a Rota

--- src/app/components/card-game/card-game.component.ts
+++ src/app/components/card-game/card-game.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from "@angular/core";
 import { gamelist } from "../../pages/home/home.component"
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { CommonModule } from "@angular/common";
 
+type gameid = string;
 @Component({
   selector: "app-card-game",
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: "./card-game.component.html",
   styleUrl: "./card-game.component.css",
 })
