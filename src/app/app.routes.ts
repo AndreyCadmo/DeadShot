41ae4e125967c8a778c46bd0defb29a38fe64b94
Andrey Cadmo <andreycadmo06@gmail.com>
2024-05-06 23:32:42 -0300

    retirando a rota para most-played

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./pages/home/home.component";
 import { JogoComponent } from "./pages/jogo/jogo.component";
-import { MostPlayedComponent } from "./pages/most-played/most-played.component";
 
 export const routes: Routes = [
   { path: "home", component: HomeComponent },
-  { path: "mais-jogados", component: MostPlayedComponent },
   { path: "jogo/:id", component: JogoComponent },
   { path: "", redirectTo: "home", pathMatch: "full" },
 ];
