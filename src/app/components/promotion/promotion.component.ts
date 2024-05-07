5f025daad25cde55d649f5f54ae85874a4cbdf14
Andrey Cadmo <andreycadmo06@gmail.com>
2024-05-06 23:34:07 -0300

    criando o componente promotion para uma dialog

---
+++ src/app/components/promotion/promotion.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
+
+@Component({
+  selector: "app-promotion",
+  standalone: true,
+  imports: [
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    MatButtonModule,
+  ],
+  templateUrl: "./promotion.component.html",
+  styleUrl: "./promotion.component.css",
+})
+export class PromotionComponent {}
